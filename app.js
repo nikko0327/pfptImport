66b972c6afe46d59679535a3d92ffaee751a1884
Nikko Lee <nlee@C02Z77WQLVDQ.corp.proofpoint.com>
2020-03-23 23:36:55 -0700

    routes for nav added

--- app.js
+++ app.js
@@ -2,16 +2,29 @@
 
 const express = require('express');
 const app = express();
-
+const bodyParser = require('body-parser');
 
 // set so we dont have to type .ejs all the time when routing
 app.set("view engine", "ejs");
 // used so we can make a public folder that contains stylesheet and js, and be able to access it
 app.use(express.static("public"));
 
-const port = 3000;
+app.use(bodyParser.urlencoded({ extended: true }));
 
+const port = process.env.PORT || 3000;
+
+//entry point of the app
 app.get("/", (req, res) => {
+  res.render("login");
+});
+
+//index page navigation
+app.get("/index", (req, res) => {
+  res.render("index");
+});
+
+//appliance page navigation
+app.get("/appliance", (req, res) => {
   res.render("appliances");
 });
 
